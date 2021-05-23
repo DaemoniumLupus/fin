b3d1a773c53ad2080d5ebb1ff5bed019a718e6e9
DaemoniumLupus <37290179+DaemoniumLupus@users.noreply.github.com>
2021-05-23 07:56:18 +0300

    Через масивы

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,10 @@
 
 
 
+int f6[10], f3[10], f5[10], f2[10]; // размеры в мм
+int naShodf5[10];
+int qf6[10], qf3[10]; //количество профиля в шт
+int qu_proemov = 0;
 
 ofstream fout;//запись в файл
 /*
@@ -26,6 +30,7 @@ struct Proem { //структура проема
 
 Proem proem = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 */
+//структура
 void zaproem(int) {
 	//Proem proem;
 	
@@ -34,10 +39,8 @@ void zaproem(int) {
 	int h; //длина и высота проема
 	int stvorkiVProem;
 	int prishepka;
-	int f6, f3 = 0, f5, f2; // размеры в мм
-	int naShodf5 = 0;
-	int qf6, qf3; //количество профиля в шт
-	int qf5 = 0;  //количество профиля в шт
+	
+	//int qf5 = 0;  //количество профиля в шт
 	int steklo[3];
 	int stekloNiz[3];
 	int qSteklo;
@@ -64,107 +67,107 @@ void zaproem(int) {
 
 	switch ( stvorkiVProem) { //количество ф6 и размеры ф3
 	case 2:
-		 qf6 = 4;
+		 qf6[qu_proemov] = 4;
 
 		if ( prishepka == 0) {
-			 f3 = ( l - 26) / 2 + 15 - 40;
+			 f3[qu_proemov] = ( l - 26) / 2 + 15 - 40;
 		}
 		else if ( prishepka == 1) {
-			 f3 = ( l - 38) / 2 + 15 - 40;
+			 f3[qu_proemov] = ( l - 38) / 2 + 15 - 40;
 		}
 		else if ( prishepka == 2) {
-			 f3 = ( l - 50) / 2 + 15 - 40;
+			 f3[qu_proemov] = ( l - 50) / 2 + 15 - 40;
 		}
 		break;
 	case 3:
-		 qf6 = 6;
+		 qf6[qu_proemov] = 6;
 
 		if ( prishepka == 0) {
-			 f3 =
+			 f3[qu_proemov] =
 				( l - 26 - 30) / 3 + 30 - 40;
 		}
 		else if ( prishepka == 1) {
-			 f3 = ( l - 38 - 30) / 3 + 30 - 40;
+			 f3[qu_proemov] = ( l - 38 - 30) / 3 + 30 - 40;
 		}
 		else if ( prishepka == 2) {
-			 f3 = ( l - 50 - 30) / 3 + 30 - 40;
+			 f3[qu_proemov] = ( l - 50 - 30) / 3 + 30 - 40;
 		}
 		break;
 	case 4:
-		 qf6 = 8;
+		 qf6[qu_proemov] = 8;
 
 		if ( prishepka == 0) {
-			 f3 = ( l - 26 - 30) / 4 + 30 - 40;
+			 f3[qu_proemov] = ( l - 26 - 30) / 4 + 30 - 40;
 		}
 		else if ( prishepka == 1) {
-			 f3 = ( l - 38 - 30) / 4 + 30 - 40;
+			 f3[qu_proemov] = ( l - 38 - 30) / 4 + 30 - 40;
 		}
 		else if ( prishepka == 2) {
-			 f3 = ( l - 50 - 30) / 4 + 30 - 40;
+			 f3[qu_proemov] = ( l - 50 - 30) / 4 + 30 - 40;
 		}
 		break;
 	case 5:
-		 qf6 = 10;
+		 qf6[qu_proemov] = 10;
 
 		if ( prishepka == 0) {
-			 f3 = ( l - 39 - 60) / 5 + 30 - 40;
+			 f3[qu_proemov] = ( l - 39 - 60) / 5 + 30 - 40;
 		}
 		else if ( prishepka == 1) {
-			 f3 = ( l - 51 - 60) / 5 + 30 - 40;
+			 f3[qu_proemov] = ( l - 51 - 60) / 5 + 30 - 40;
 		}
 		else if ( prishepka == 2) {
-			 f3 = ( l - 63 - 60) / 5 + 30 - 40;
+			 f3[qu_proemov] = ( l - 63 - 60) / 5 + 30 - 40;
 		}
 		else if ( prishepka == 3) {
-			 f3 = ( l - 75 - 60) / 5 + 30 - 40;
+			 f3[qu_proemov] = ( l - 75 - 60) / 5 + 30 - 40;
 		}
 		break;
 	case 6:
-		 qf6 = 12;
+		 qf6[qu_proemov] = 12;
 
 		if ( prishepka == 0) {
-			 f3 = ( l - 39 - 60) / 6 + 30 - 40;
+			 f3[qu_proemov] = ( l - 39 - 60) / 6 + 30 - 40;
 		}
 		else if ( prishepka == 1) {
-			 f3 = ( l - 51 - 60) / 6 + 30 - 40;
+			 f3[qu_proemov] = ( l - 51 - 60) / 6 + 30 - 40;
 		}
 		else if ( prishepka == 2) {
-			 f3 = ( l - 63 - 60) / 6 + 30 - 40;
+			 f3[qu_proemov] = ( l - 63 - 60) / 6 + 30 - 40;
 		}
 		else if ( prishepka == 3) {
-			 f3 = ( l - 75 - 60) / 6 + 30 - 40;
+			 f3[qu_proemov] = ( l - 75 - 60) / 6 + 30 - 40;
 		}
 		break;
 	case 7:
-		 qf6 = 14;
+		 qf6[qu_proemov] = 14;
 
 		if ( prishepka == 0) {
-			 f3 = ( l - 39 - 60) / 7 + 30 - 40;
+			 f3[qu_proemov] = ( l - 39 - 60) / 7 + 30 - 40;
 		}
 		else if ( prishepka == 1) {
-			 f3 = ( l - 51 - 60) / 7 + 30 - 40;
+			 f3[qu_proemov] = ( l - 51 - 60) / 7 + 30 - 40;
 		}
 		else if ( prishepka == 2) {
-			 f3 = ( l - 63 - 60) / 7 + 30 - 40;
+			 f3[qu_proemov] = ( l - 63 - 60) / 7 + 30 - 40;
 		}
 		else if ( prishepka == 3) {
-			 f3 = ( l - 75 - 60) / 7 + 30 - 40;
+			 f3[qu_proemov] = ( l - 75 - 60) / 7 + 30 - 40;
 		}
 		break;
 	case 8:
-		 qf6 = 16;
+		 qf6[qu_proemov] = 16;
 
 		if ( prishepka == 0) {
-			 f3 = ( l - 39 - 60) / 8 + 30 - 40;
+			 f3[qu_proemov] = ( l - 39 - 60) / 8 + 30 - 40;
 		}
 		else if ( prishepka == 1) {
-			 f3 = ( l - 51 - 60) / 8 + 30 - 40;
+			 f3[qu_proemov] = ( l - 51 - 60) / 8 + 30 - 40;
 		}
 		else if ( prishepka == 2) {
-			 f3 = ( l - 63 - 60) / 8 + 30 - 40;
+			 f3[qu_proemov] = ( l - 63 - 60) / 8 + 30 - 40;
 		}
 		else if ( prishepka == 3) {
-			 f3 = ( l - 75 - 60) / 8 + 30 - 40;
+			 f3[qu_proemov] = ( l - 75 - 60) / 8 + 30 - 40;
 		}
 		break;
 	default:
@@ -173,46 +176,46 @@ void zaproem(int) {
 
 	if ( stvorkiVProem > 4) { //размеры и количество ф5 на сход
 
-		 naShodf5 =  h - 38;
+		 naShodf5[qu_proemov] =  h - 38;
 	}
 	if ( impost) { //количество ф3
-		 qf3 =  stvorkiVProem * 3;
+		 qf3[qu_proemov] =  stvorkiVProem * 3;
 	}
 	else
-		 qf3 =  stvorkiVProem * 2;
+		 qf3[qu_proemov] =  stvorkiVProem * 2;
 
-	 f2 =  l; //размеры ф2
+	 f2[qu_proemov] =  l; //размеры ф2
 
-	 f6 =  h - 38; //размеры ф6
-	 f5 =  h - 16; //размеры ф5
+	 f6[qu_proemov] =  h - 38; //размеры ф6
+	 f5[qu_proemov] =  h - 16; //размеры ф5
 
   //cout << "\nalarm" << proem[qu_proemov].qf6 << endl << proem[qu_proemov].f5 << endl << proem[qu_proemov].qf5;
 
-	 steklo[0] =  f3 - 2; //ширина стекла
-	 stekloNiz[0] =  steklo[0];
+	 steklo[0] =  f3[qu_proemov] - 2; //ширина стекла
+	 
 	 qSteklo =  stvorkiVProem;
 
 
 	if ( impost) {
-		 steklo[1] =  f6 -  verhImposta - 34;
+		 steklo[1] =  f6[qu_proemov] -  verhImposta - 34;
 		 stekloNiz[1] =  verhImposta - 43 - 33;
+		 stekloNiz[0] = steklo[0];
 		 qStekloNiz =  stvorkiVProem;
 	}
-	else  steklo[1] =  f6 - 86; //высота стекла
+	else  steklo[1] =  f6[qu_proemov] - 86; //высота стекла
 
 //cout << "\nalarm" << proem[qu_proemov].qf6 << endl << proem[qu_proemov].f5 << endl << proem[qu_proemov].qf5;
 
-	cout << "f2 - " <<  f2 << " - 2 шт.\n";
-	cout << "f3 - " <<  f3 << " - " <<  qf3
+	cout << "f2 - " <<  f2[qu_proemov] << " - 2 шт.\n";
+	cout << "f3 - " <<  f3[qu_proemov] << " - " <<  qf3[qu_proemov]
 		<< " шт.\n";
-	cout << "f5 - " <<  f5 << " - " <<  qf5
-		<< " шт.\n";
+	cout << "f5 - " <<  f5[qu_proemov] << " - "/* <<  qf5*/ << "2 шт.\n";
 	//cout << "\nalarm" << proem[qu_proemov].qf6 << endl << proem[qu_proemov].f5 << endl << proem[qu_proemov].qf5;
-	if ( naShodf5 > 0) {
-		cout << "f5 - " <<  naShodf5 << " - 1 шт.\n";
+	if ( naShodf5[qu_proemov] > 0) {
+		cout << "f5 - " <<  naShodf5[qu_proemov] << " - 1 шт.\n";
 	}
 	//cout << "\nalarm" << proem[qu_proemov].qf6 << endl <<proem[qu_proemov].f5 << endl <<proem[qu_proemov].qf5<<"\n";
-	cout << "f6 - " <<  f6 << " - " <<  qf6 << " шт.\n";
+	cout << "f6 - " <<  f6[qu_proemov] << " - " <<  qf6[qu_proemov] << " шт.\n";
 	for (int i = 0; i < 10; i++) {
 		cout << "~";
 	}
@@ -226,17 +229,17 @@ void zaproem(int) {
 	}
 
 	fout.open("text.txt", ios::app);
-	fout << "f2 - " <<  f2 << " - 2 шт.\n";
-	fout << "f3 - " <<  f3 << " - " <<  qf3
+	fout << "\nProem " << qu_proemov + 1;
+	fout << "\nf2 - " <<  f2[qu_proemov] << " - 2 шт.\n";
+	fout << "f3 - " <<  f3[qu_proemov] << " - " <<  qf3[qu_proemov]
 		<< " шт.\n";
-	fout << "f5 - " <<  f5 << " - " <<  qf5
-		<< " шт.\n";
+	fout << "f5 - " <<  f5[qu_proemov] << " - "/* <<  qf5*/<< " шт.\n";
 	//cout << "\nalarm" << proem[qu_proemov].qf6 << endl << proem[qu_proemov].f5 << endl << proem[qu_proemov].qf5;
-	if ( naShodf5 > 0) {
-		fout << "f5 - " <<  naShodf5 << " - 1 шт.\n";
+	if ( naShodf5[qu_proemov] > 0) {
+		fout << "f5 - " <<  naShodf5[qu_proemov] << " - 1 шт.\n";
 	}
 	//cout << "\nalarm" << proem[qu_proemov].qf6 << endl <<proem[qu_proemov].f5 << endl <<proem[qu_proemov].qf5<<"\n";
-	fout << "f6 - " <<  f6 << " - " <<  qf6 << " шт.\n";
+	fout << "f6 - " <<  f6[qu_proemov] << " - " <<  qf6[qu_proemov] << " шт.\n";
 	
 	fout << "\nsteklo - " <<  steklo[0] << "x"
 		<<  steklo[1] << " - " <<  qSteklo
@@ -259,7 +262,7 @@ int main() {
 	//int qu_proemov; // счетчик для контроля номера проема
 	cout << "kol-vo proemov\n";
 	cin >> i;
-	for (int qu_proemov = 0; qu_proemov < i; qu_proemov++) {
+	for (; qu_proemov < i; qu_proemov++) {
 		/*
 		 impost = 0;
 		 l = 0;
@@ -285,7 +288,7 @@ int main() {
 		
 		
 	}
-	system("print text.txt");
+	//system("print text.txt");
 
 	system("pause");
 	return 0;
